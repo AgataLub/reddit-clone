8e3a2df3a289568094367b25ffc96bc986a4a324
Agata <agat0538@stud.kea.dk>
2021-10-21 13:06:09 +0200

    created utils

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
 import PostList from "./PostList";
+import { constants, fetchData } from ".utils";
 
 function App() {
   //States
   const [feed, setFeed] = useState([]);
 
   //Fetching data
-  async function getData() {
-    const response = await fetch("https://www.reddit.com/.json", {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Accept: "application/json",
-      },
-    });
-    const myJson = await response.json();
-    return myJson;
-  }
-  getData().then((myJson) => {
+  // async function getData() {
+  //   const response = await fetch("https://www.reddit.com/.json", {
+  //     headers: {
+  //       "Content-Type": "application/x-www-form-urlencoded",
+  //       Accept: "application/json",
+  //     },
+  //   });
+  //   const myJson = await response.json();
+  //   return myJson;
+  // }
+
+  fetchData(constants.redditURL);
+
+  fetchData().then((myJson) => {
     console.log(myJson.data.children);
     setFeed(myJson.data.children);
   });
